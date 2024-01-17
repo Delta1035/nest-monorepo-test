af724754de4da503a0620f1b03279c6081478f15
Delta1035 <528491526@qq.com>
2024-01-17 17:47:50 +0800

    test

--- apps/jwt-and-session/src/jwt-and-session.service.ts
+++ apps/jwt-and-session/src/jwt-and-session.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class JwtAndSessionService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!!';
   }
 }
